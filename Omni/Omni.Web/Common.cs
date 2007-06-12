de4469619dc4e3454ab430b5e62512e1e2609250
mscoder610 <mscoder610@c9275b37-ef31-0410-ace8-1ff643ab3d93>
2007-06-12 03:59:17 +0000

    MD5 code added

--- Omni/Omni.Web/Common.cs
+++ Omni/Omni.Web/Common.cs
@@ -18,5 +18,12 @@ namespace Omni.Web
             if (HttpContext.Current.Session==null||HttpContext.Current.Session["PublicWebService"] == null) throw new NullReferenceException("No session used.");
             return (org.omniproject.service.PublicWebService)HttpContext.Current.Session["PublicWebService"];
         }
+
+        public static string GetMD5Hash(string message)
+        {
+            System.Security.Cryptography.MD5 hash = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            hash.Initialize();
+            return BitConverter.ToString(hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(message))).ToLower();
+        }
     }
 }
