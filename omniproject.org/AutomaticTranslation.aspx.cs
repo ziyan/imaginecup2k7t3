863a8cb788e988e54a4b5df0baa3ff87213a573d
alfred.leung <alfred.leung@c9275b37-ef31-0410-ace8-1ff643ab3d93>
2007-06-12 08:07:59 +0000

    - less hardcoding for the automatic translations

--- omniproject.org/AutomaticTranslation.aspx.cs
+++ omniproject.org/AutomaticTranslation.aspx.cs
@@ -9,26 +9,47 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 
+using Omni.Web;
+using Omni.Web.org.omniproject;
+
+
 public partial class AutomaticTranslation : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        // FIXME: this is hardcoded right now: we'll need to localize this
         if ((sourceDropDown.Items.Count == 0) && 
                 (destinationDropDown.Items.Count == 0))
         {
-            sourceDropDown.Items.Add("English");
-            sourceDropDown.Items.Add("Chinese");
-            destinationDropDown.Items.Add("English");
-            destinationDropDown.Items.Add("Chinese");
+            Language[] languages = Common.GetWebService().LanguageList();
+
+            // FIXME: get site language
+            int defaultLanguageId = -1;
+            foreach (Language language in languages)
+            {
+                if (language.code.Equals("ENG"))
+                {
+                    defaultLanguageId = language.id;
+                    break;
+                }
+            }
+            if (defaultLanguageId == -1) return; // can't do anything
+
+            foreach (Language language in languages)
+            {
+                string languageString = Common.GetWebService().LanguageNameQueryById(
+                        language.id, defaultLanguageId);
+                sourceDropDown.Items.Add(
+                            new ListItem(languageString, language.id.ToString()));
+                destinationDropDown.Items.Add(languageString);
+            }
         }
     }
     protected void TranslationButton_Click(object sender, EventArgs e)
     {
         ListItem sourceItem = sourceDropDown.Items[sourceDropDown.SelectedIndex];
         ListItem destItem = destinationDropDown.Items[destinationDropDown.SelectedIndex];
-        int sourceCode = sourceItem.Text.Equals("English") ? 1 : 2;
-        int destCode = destItem.Text.Equals("English") ? 1 : 2;
+        int sourceCode = Convert.ToInt32(sourceItem.Value);
+        int destCode = Convert.ToInt32(destItem.Value);
 
         string translatedText= Omni.Web.Common.GetWebService().TranslationLookup(
                 sourceCode, destCode, OriginalMessageText.Text);
