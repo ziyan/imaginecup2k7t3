f299719480634d54e33459439b60fab9d87dcdf7
alfred.leung <alfred.leung@c9275b37-ef31-0410-ace8-1ff643ab3d93>
2007-06-12 23:24:36 +0000

    Prettier interest picker

--- omniproject.org/InterestsPicker.ascx
+++ omniproject.org/InterestsPicker.ascx
@@ -1,49 +1,5 @@
 <%@ Control Language="C#" AutoEventWireup="true" CodeFile="InterestsPicker.ascx.cs" Inherits="InterestsPicker" %>
 <asp:Label ID="interestsLabel" runat="server" Text="Interests"></asp:Label>
 <br />
-<asp:Table ID="outterTable" runat="server">
-    <asp:TableRow runat="server">
-        <asp:TableCell runat="server">
-            <asp:Table ID="mineTable" runat="server">
-                <asp:TableRow runat="server">
-                    <asp:TableCell runat="server">
-                        <asp:Label ID="mineLable" runat="server" Text="Mine"></asp:Label>
-                    </asp:TableCell>
-                </asp:TableRow>
-                <asp:TableRow runat="server">
-                    <asp:TableCell runat="server">
-                        <asp:ListBox ID="mineListBox" runat="server"></asp:ListBox>
-                    </asp:TableCell>
-                </asp:TableRow>
-            </asp:Table>
-        </asp:TableCell>
-        <asp:TableCell runat="server">
-            <asp:Table ID="controlTable" runat="server">
-                <asp:TableRow runat="server">
-                    <asp:TableCell runat="server">
-                        <asp:Button ID="addButton" runat="server" Text="&lt; Add" OnClick="Add_Click" />
-                    </asp:TableCell>
-                </asp:TableRow>
-                <asp:TableRow runat="server">
-                    <asp:TableCell runat="server">
-                        <asp:Button ID="removeButton" runat="server" Text="Remove &gt;" OnClick="Remove_Click" />
-                    </asp:TableCell>
-                </asp:TableRow>
-            </asp:Table>
-        </asp:TableCell>
-        <asp:TableCell runat="server">
-            <asp:Table ID="allTable" runat="server">
-                <asp:TableRow runat="server">
-                    <asp:TableCell runat="server">
-                        <asp:Label ID="allLabel" runat="server" Text="All"></asp:Label>
-                    </asp:TableCell>
-                </asp:TableRow>
-                <asp:TableRow runat="server">
-                    <asp:TableCell runat="server">
-                        <asp:ListBox ID="allListBox" runat="server"></asp:ListBox>
-                    </asp:TableCell>
-                </asp:TableRow>
-            </asp:Table>
-        </asp:TableCell>
-    </asp:TableRow>
-</asp:Table>
+<asp:TreeView ID="allTreeView" runat="server"></asp:TreeView>
+
--- omniproject.org/InterestsPicker.ascx.cs
+++ omniproject.org/InterestsPicker.ascx.cs
@@ -9,38 +9,42 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 
+using Omni.Web;
+using Omni.Web.org.omniproject;
+
 public partial class InterestsPicker : System.Web.UI.UserControl
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        // FIXME: get data from database
-        if ((allListBox.Items.Count == 0) && (mineListBox.Items.Count == 0))
+        int preferredLanguageId = Common.GetPreferredLanguage();
+        if (preferredLanguageId <= 0) return; // should not happen
+
+        WebService service = Common.GetWebService();
+
+        // UserGetUserInterest
+        //Common.GetCurrentUser().id
+
+        Interest[] categories = service.InterestList(0);
+        foreach (Interest category in categories)
         {
-            allListBox.Items.Add("Interest1");
-            allListBox.Items.Add("Interest2");
-            allListBox.Items.Add("Interest3");
-            mineListBox.Items.Add("InterestA");
-            mineListBox.Items.Add("InterestB");
+            string categoryString = 
+                    service.InterestNameQueryById(category.id, preferredLanguageId);
+            TreeNode categoryNode = new TreeNode(categoryString);
+            categoryNode.ShowCheckBox = true;
+
+            Interest[] interests = service.InterestList(category.id);
+            foreach (Interest interest in interests)
+            {
+                string interestString = service.InterestNameQueryById(interest.id, 
+                        preferredLanguageId);
+
+                TreeNode interestNode = new TreeNode(interestString);
+                interestNode.ShowCheckBox = true;
+                categoryNode.ChildNodes.Add(interestNode);
+            }
+
+            categoryNode.Collapse();
+            allTreeView.Nodes.Add(categoryNode);
         }
-    }
-
-    protected void Add_Click(object sender, EventArgs e)
-    {
-        if (allListBox.SelectedIndex == -1) return;
-
-        ListItem selectedItem = allListBox.Items[allListBox.SelectedIndex];
-        selectedItem.Selected = false;
-        mineListBox.Items.Add(selectedItem);
-        allListBox.Items.Remove(selectedItem);
-    }
-
-    protected void Remove_Click(object sender, EventArgs e)
-    {
-        if (mineListBox.SelectedIndex == -1) return;
-
-        ListItem selectedItem = mineListBox.Items[mineListBox.SelectedIndex];
-        selectedItem.Selected = false;
-        allListBox.Items.Add(selectedItem);
-        mineListBox.Items.Remove(selectedItem);
     }
 }
